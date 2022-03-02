b45fc6574b0191f2764ff4c719da8134b521eb4d
quarufus <93532038+quarufus@users.noreply.github.com>
2022-03-02 15:38:10 +0200

    Add files via upload

---
+++ src/library.rs
@@ -0,0 +1,303 @@
+use tui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Style,
+    text::{Spans, Text},
+    widgets::{Block, StatefulWidget, Widget},
+};
+use std::str::FromStr;
+use std::num::ParseIntError;
+
+#[derive(Clone, Copy)]
+pub struct LibState {
+    offset: usize,
+    selected: Option<usize>,
+    playing: Option<usize>,
+}
+
+impl Default for LibState {
+    fn default() -> LibState {
+        LibState {
+            offset: 0,
+            selected: Some(0),
+            playing: None,
+        }
+    }
+}
+
+impl LibState {
+    pub fn selected(&self) -> Option<usize> {
+        self.selected
+    }
+
+    pub fn playing(&self) -> Option<usize> {
+        self.playing
+    }
+
+    pub fn select(&mut self, index: Option<usize>) {
+        self.selected = index;
+        if index.is_none() {
+            self.offset = 0;
+        }
+    }
+
+    pub fn set_playing(&mut self, index: Option<usize>) {
+        self.playing = index
+    }
+
+    pub fn offset(&mut self, index: usize) {
+        self.offset = index;
+    }
+}
+
+#[derive(PartialEq)]
+pub enum LibKind {
+    Artist,
+    Album,
+    Title,
+    Playlist,
+    Back,
+    Home,
+    Option,
+    None,
+}
+
+impl Copy for LibKind {}
+
+impl Clone for LibKind {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+#[derive(Clone)]
+pub struct LibItem {
+    pub content: String,
+    pub style: Style,
+    pub tag: LibKind,
+}
+
+impl FromStr for LibItem {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(LibItem {
+            content: s.into(),
+            style: Style::default(),
+            tag: LibKind::None,
+        })
+    }
+}
+
+/*impl Iterator for LibItem {
+    type item = usize;
+
+    fn next(&mut self) -> Option<Self::item> {
+        self.count += 1;
+
+        if self*/
+
+impl LibItem {
+    pub fn new(content: String, tag: LibKind) -> LibItem {
+        LibItem {
+            content,
+            style: Style::default(),
+            tag,
+        }
+    }
+
+    pub fn change_tag(&mut self, tag: LibKind) {
+        self.tag = tag
+    }
+}
+
+pub struct Tree<'a> {
+    block: Option<Block<'a>>,
+    items: &'a Vec<LibItem>,
+    style: Style,
+    highlight_style: Style,
+    highlight_symbol: Option<&'a str>,
+    playing_style: Style,
+    playing_symbol: Option<&'a str>,
+}
+
+impl<'a> Tree<'a> {
+    pub fn new<T>(items: T) -> Tree<'a>
+    where
+        T: Into<&'a Vec<LibItem>>,
+    {
+        Tree {
+            block: None,
+            style: Style::default(),
+            items: &items.into(),
+            highlight_style: Style::default(),
+            highlight_symbol: None,
+            playing_style: Style::default(),
+            playing_symbol: None,
+        }
+    }
+
+    pub fn block(mut self, block: Block<'a>) -> Tree<'a> {
+        self.block = Some(block);
+        self
+    }
+
+    pub fn style(mut self, style: Style) -> Tree<'a> {
+        self.style = style;
+        self
+    }
+
+    pub fn highlight_symbol(mut self, highlight_symbol: &'a str) -> Tree<'a> {
+        self.highlight_symbol = Some(highlight_symbol);
+        self
+    }
+
+    pub fn highlight_style(mut self, style: Style) -> Tree<'a> {
+        self.highlight_style = style;
+        self
+    }
+
+    pub fn playing_style(mut self, style:Style) -> Tree<'a> {
+        self.playing_style = style;
+        self
+    }
+
+    pub fn playing_symbol(mut self, symbol: &'a str) -> Tree<'a> {
+        self.playing_symbol = Some(symbol);
+        self
+    }
+
+    fn get_items_bounds(
+        &self,
+        selected: Option<usize>,
+        offset: usize,
+        max_height: usize,
+    ) -> (usize, usize) {
+        let offset = offset.min(self.items.len().saturating_sub(1));
+        let mut start = offset;
+        let mut end = offset;
+        let mut height = 0;
+        for item in self.items.iter().skip(offset) {
+            if height + Text::from(item.content.clone()).height() > max_height {
+                break;
+            }
+            height += Text::from(item.content.clone()).height();
+            end += 1;
+        }
+
+        let selected = selected.unwrap_or(0).min(self.items.len() - 1);
+        while selected >= end {
+            height = height.saturating_add(Text::from(self.items[end].content.clone()).height());
+            end += 1;
+            while height > max_height {
+                height =
+                    height.saturating_sub(Text::from(self.items[start].content.clone()).height());
+                start += 1;
+            }
+        }
+        while selected < start {
+            start -= 1;
+            height = height.saturating_add(Text::from(self.items[start].content.clone()).height());
+            while height > max_height {
+                end -= 1;
+                height =
+                    height.saturating_sub(Text::from(self.items[end].content.clone()).height());
+            }
+        }
+        (start, end)
+    }
+}
+
+impl<'a> StatefulWidget for Tree<'a> {
+    type State = LibState;
+
+    fn render(mut self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        buf.set_style(area, self.style);
+        let list_area = match self.block.take() {
+            Some(b) => {
+                let inner_area = b.inner(area);
+                b.render(area, buf);
+                inner_area
+            }
+            None => area,
+        };
+
+        if list_area.width < 1 || list_area.height < 1 {
+            return;
+        }
+
+        if self.items.is_empty() {
+            return;
+        }
+        let list_height = list_area.height as usize;
+
+        let (start, end) = self.get_items_bounds(state.selected, state.offset, list_height);
+        state.offset = start;
+
+        let highlight_symbol = self.highlight_symbol.unwrap_or("");
+
+        let mut current_height = 0;
+        let has_selection = state.selected.is_some();
+        for (i, item) in self
+            .items
+            .iter()
+            .enumerate()
+            .skip(state.offset)
+            .take(end - start)
+        {
+            let (x, y) = (list_area.left(), list_area.top() + current_height);
+            current_height += Text::from(item.content.clone()).height() as u16;
+            let area = Rect {
+                x,
+                y,
+                width: list_area.width,
+                height: Text::from(item.content.clone()).height() as u16,
+            };
+
+            let item_style = self.style.patch(item.style);
+            buf.set_style(area, item_style);
+
+            let is_selected = state.selected.map(|s| s == i).unwrap_or(false);
+            let is_playing = state.playing.map(|s| s == i).unwrap_or(false);
+
+            let isymbol = match item.tag {
+                LibKind::Artist => String::from("   "),
+                LibKind::Album => String::from("   "),
+                LibKind::Title => String::from("   "),
+                LibKind::Home => String::from(" "),
+                LibKind::None => String::from(" "),
+                _ => String::from(""),
+            };
+            let (elem_x, max_element_width) = if has_selection {
+                let symbol = if is_playing {
+                    self.playing_symbol.unwrap_or("   ")
+                } else {
+                    &isymbol
+                };
+                let (elem_x, _) =
+                        buf.set_stringn(x, y, &symbol, list_area.width as usize, item_style);
+                (elem_x, (list_area.width - (elem_x - x)) as u16)
+            } else {
+                (x, list_area.width)
+            };
+            buf.set_spans(
+                elem_x,
+                y,
+                &Spans::from(item.content.clone()),
+                max_element_width as u16,
+            );
+            let rect = Rect::new(
+                x,//isymbol.chars().count() as u16,
+                y,
+                list_area.width,// - isymbol.chars().count() as u16,
+                1,
+            );
+            if is_selected {
+                buf.set_style(rect, self.highlight_style);
+            }
+            if is_playing {
+                buf.set_style(rect, self.playing_style);
+            }
+        }
+    }
+}
---
+++ src/ui.rs
@@ -0,0 +1,315 @@
+use crate::library::{Tree, LibKind};
+pub use crate::libs::Data;
+use crate::position::PositionWidget;
+
+use mpd::status::{State, Status};
+use mpd::song::QueuePlace;
+
+use tui::{
+    backend::Backend,
+    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    style::{Style, Modifier},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph, Tabs},
+    Frame,
+};
+
+pub fn draw<B: Backend>(f: &mut Frame<B>, data: &mut Data) {
+    let block = Block::default().borders(Borders::NONE).style(data.style);
+    f.render_widget(block, f.size());
+
+    let chunks = Layout::default()
+        .constraints(
+            [
+                Constraint::Length(2),
+                Constraint::Min(0),
+                Constraint::Length(1),
+                Constraint::Length(1),
+            ]
+            .as_ref(),
+        )
+        .horizontal_margin(0)
+        .split(f.size());
+    let top = Layout::default()
+        .constraints([Constraint::Min(0), Constraint::Length(5)].as_ref())
+        .direction(Direction::Horizontal)
+        .horizontal_margin(0)
+        .split(chunks[0]);
+    let tabtitles = [status_icon(&data.status), " ", " ", " "]
+        .iter()
+        .cloned()
+        .map(|t| Spans::from(Span::styled(t, data.style)))
+        .collect();
+    let tabs = Tabs::new(tabtitles)
+        .block(Block::default().borders(Borders::BOTTOM))
+        .select(data.tabindex)
+        .style(data.style)
+        .highlight_style(Style::default().fg(data.colors.highlight))
+        .divider("");
+    f.render_widget(tabs, top[0]);
+
+    draw_volume(f, data, top[1]);
+
+    match data.tabindex {
+        0 => draw_current(f, data, chunks[1]),
+        1 => draw_library(f, data, chunks[1]),
+        2 => draw_queue(f, data, chunks[1]),
+        3 => draw_settings(f, data, chunks[1]),
+        5 => draw_options(f, data, chunks[1]),
+        _ => {}
+    }
+    if data.options {
+        draw_options(f, data, chunks[1]);
+    }        
+
+    draw_position(f, &data, chunks[2]);
+
+    draw_status_bar(f, &data, chunks[3]);
+}
+
+fn draw_volume<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    let volume = data.status.volume.to_string();
+    let text = vec![Spans::from(vec![Span::from(volume), Span::from("% ")])];
+    let volume = Paragraph::new(text)
+        .block(Block::default().style(data.style).borders(Borders::BOTTOM))
+        .alignment(Alignment::Left);
+    f.render_widget(volume, area);
+}
+
+fn draw_current<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    let area = Layout::default()
+        .constraints([Constraint::Min(0), Constraint::Length(1)].as_ref())
+        .horizontal_margin(1)
+        .split(area);
+    let default_artist = String::from("Unknown Artist");
+    let default_album = String::from("Uknown Album");
+    let artist_text = data.current.tags.get("Artist").unwrap_or(&default_artist);
+    let album_text = data.current.tags.get("Album").unwrap_or(&default_album);
+    let text = vec![
+        Spans::from(Span::styled(
+            " Artist:",
+            Style::default().fg(data.colors.highlight),
+        )),
+        Spans::from(vec![Span::from(""), Span::from(artist_text.clone())]),
+        Spans::from(Span::styled(
+            " Album:",
+            Style::default().fg(data.colors.highlight),
+        )),
+        Spans::from(vec![Span::from(""), Span::from(album_text.clone())]),
+        //Spans::from(repeat_shuffle(&data.status)),
+    ];
+    let paragraph = Paragraph::new(text)
+        .block(Block::default().style(data.style))
+        .alignment(Alignment::Center);
+    f.render_widget(paragraph, area[0]);
+
+    let chunks = Layout::default()
+        .constraints(
+            [
+                Constraint::Length(5),
+                Constraint::Min(4),
+                Constraint::Length(5),
+            ]
+            .as_ref(),
+        )
+        .direction(Direction::Horizontal)
+        .split(area[1]);
+
+    let time = time::Duration::seconds(0);
+    let status = &data.status;
+    let elapsedmin = status.elapsed.unwrap_or(time).num_minutes();
+    let elapsedsec = status.elapsed.unwrap_or(time).num_seconds() % 60;
+    let durationmin = status.duration.unwrap_or(time).num_minutes();
+    let durationsec = status.duration.unwrap_or(time).num_seconds() % 60;
+
+    let elapsed = format!("{:0>2}:{:0>2}", elapsedmin, elapsedsec);
+    let duration = format!("{:0>2}:{:0>2} ", durationmin, durationsec);
+    let text = Paragraph::new(elapsed)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .alignment(Alignment::Left);
+    f.render_widget(text, chunks[0]);
+
+    let text = Paragraph::new(duration)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .alignment(Alignment::Right);
+    f.render_widget(text, chunks[2]);
+
+}
+
+fn draw_library<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    let mut n = 0;
+    for item in &mut data.library.items {
+        if &item.content == &data.current.title.clone().unwrap_or("".into()) { data.library.state.set_playing(Some(n)) }
+        n += 1;
+    }
+    let list = Tree::new(&data.library.items)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .highlight_style(Style::default().bg(data.colors.selected))//fg(data.colors.highlight))
+        .playing_symbol("   ")
+        .playing_style(Style::default().fg(data.colors.highlight));
+    f.render_stateful_widget(list, area, &mut data.library.state);
+}
+
+fn draw_queue<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    /*let mut i = 0;
+    for song in data.queue.items.iter() {
+        if data.status.song.unwrap_or(QueuePlace::default()).pos as usize > 0 {
+            i = data.status.song.unwrap_or(QueuePlace::default()).pos as usize;
+        }
+    }
+    data.drained = i;
+    data.queue.items.drain(0..i);*/
+
+    let list = Tree::new(&data.queue.items)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .highlight_style(Style::default().bg(data.colors.selected))
+        .playing_style(Style::default().fg(data.colors.highlight))
+        .playing_symbol("   ");
+    f.render_stateful_widget(list, area, &mut data.queue.state);
+}
+
+fn draw_settings<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    let layout = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .horizontal_margin(3)
+        .split(area);
+    let block = Block::default().style(data.style).borders(Borders::NONE);
+    let items: Vec<ListItem> = data
+        .settings
+        .items
+        .iter()
+        .map(|i| ListItem::new(Spans::from(i.clone())))
+        .collect();
+    let list = List::new(items)
+        .block(Block::default().style(data.style).borders(Borders::ALL))
+        .highlight_style(Style::default().fg(data.colors.highlight));
+    f.render_widget(block, area);
+    f.render_stateful_widget(list, layout[0], &mut data.settings.state)
+}
+
+fn draw_position<B>(f: &mut Frame<B>, data: &Data, area: Rect)
+where
+    B: Backend,
+{
+    let chunks = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .horizontal_margin(0)
+        .split(area);
+    let status = &data.status;
+    let pos = match status.elapsed {
+        Some(p) => p.num_seconds(),
+        None => 0,
+    };
+    let full = match status.duration {
+        Some(f) => f.num_seconds(),
+        None => 1,
+    };
+
+    let gauge = PositionWidget::default()
+        .block(Block::default().borders(Borders::NONE))
+        .style(
+            Style::default()
+                .fg(data.colors.selected)
+                .bg(data.colors.background),
+        )
+        .ratio(pos as f64 / full as f64);
+    f.render_widget(gauge, chunks[0]);
+}
+
+fn draw_status_bar<B>(f: &mut Frame<B>, data: &Data, area: Rect)
+where
+    B: Backend,
+{
+    let chunks = Layout::default()
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Min(4),
+                Constraint::Length(1),
+            ]
+            .as_ref(),
+        )
+        .direction(Direction::Horizontal)
+        .split(area);
+
+    let time = time::Duration::seconds(0);
+    let status = &data.status;
+    let elapsedmin = status.elapsed.unwrap_or(time).num_minutes();
+    let elapsedsec = status.elapsed.unwrap_or(time).num_seconds() % 60;
+    let durationmin = status.duration.unwrap_or(time).num_minutes();
+    let durationsec = status.duration.unwrap_or(time).num_seconds() % 60;
+
+    let elapsed = format!(" {:0>2}:{:0>2}", elapsedmin, elapsedsec);
+    let duration = format!("{:0>2}:{:0>2} ", durationmin, durationsec);
+    let text = Paragraph::new(elapsed)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .alignment(Alignment::Left);
+    //f.render_widget(text, chunks[0]);
+
+    let text = Paragraph::new(duration)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .alignment(Alignment::Right);
+    //f.render_widget(text, chunks[2]);
+
+    let current = match data.current.title.clone() {
+        Some(song) => String::from(song),
+        None => String::from(""),
+    };
+    let text = Paragraph::new(current)
+        .block(Block::default().style(data.style).borders(Borders::NONE))
+        .alignment(Alignment::Center);
+    f.render_widget(text, chunks[1]);
+}
+
+fn draw_options<B>(f: &mut Frame<B>, data: &mut Data, area: Rect)
+where
+    B: Backend,
+{
+    let layout = Layout::default()
+        .constraints([Constraint::Percentage(100)])
+        .vertical_margin(4)
+        .horizontal_margin(6)
+        .split(area);
+    let block = Block::default().style(data.style).borders(Borders::NONE);
+    let list = Tree::new(&data.opts.items)
+        .block(Block::default().style(data.style).borders(Borders::ALL).border_type(tui::widgets::BorderType::Rounded))
+        .highlight_style(Style::default().bg(data.colors.selected));
+    f.render_widget(block, layout[0]);
+    f.render_stateful_widget(list, layout[0], &mut data.opts.state)
+}
+
+fn status_icon<'a>(status: &Status) -> &'a str {
+    let currently: &str;
+    if status.state == State::Play {
+        currently = " ";
+        &currently[..]
+    } else {
+        currently = " ";
+        &currently[..]
+    }
+}
+
+fn repeat_shuffle<'a>(status: &Status) -> Vec<Span> {
+    let mut out: &str = "";
+    if status.repeat {
+        out = "凌"
+    } else if !status.single {
+        out = " 綾"
+    }
+    vec![Span::from(out)]
+}
